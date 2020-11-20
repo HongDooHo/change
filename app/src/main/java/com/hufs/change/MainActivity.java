2e432f474b32843d07299e4fb74471a57a456a69
duri <kdle_@naver.com>
2020-11-20 23:49:08 +0900

    map marker code

--- app/src/main/java/com/hufs/change/MainActivity.java
+++ app/src/main/java/com/hufs/change/MainActivity.java
@@ -1,34 +1,61 @@
 package com.hufs.change;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.hufs.change.R;
 
 import com.hufs.change.FirstFragment;
 import com.hufs.change.SecondFragment;
 import com.hufs.change.ThirdFragment;
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import me.relex.circleindicator.CircleIndicator;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, AutoPermissionsListener {
 
     private GoogleMap mMap;
+
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+    private Marker currentMarker = null;
 
     FragmentPagerAdapter adapterViewPager;
 
@@ -39,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
         adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
-        vpPager.setAdapter(adapterViewPager);                                       
+        vpPager.setAdapter(adapterViewPager);
 
         CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
         indicator.setViewPager(vpPager);
@@ -49,8 +76,42 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 3;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return FirstFragment.newInstance(0, "Page # 1");
+                case 1:
+                    return SecondFragment.newInstance(1, "Page # 2");
+                case 2:
+                    return ThirdFragment.newInstance(2, "Page # 3");
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Page " + position;
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        // style
         try {
             // Customise the styling of the base map using a JSON object defined
             // in a raw resource file.
@@ -66,43 +127,157 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
         mMap = googleMap;
+        startLocationService();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+        try {
+            JSONArray jsonArray = new JSONArray(getJsonString());
+            for(int i =0;i<jsonArray.length();i++) {
+                JSONObject jObject = jsonArray.getJSONObject(i);
+                String name = jObject.getString("name");
+                int lon = jObject.getInt("longitude");
+                int lat = jObject.getInt("latitude");
+                MarkerOptions markerOptions = new MarkerOptions();
 
-    public static class MyPagerAdapter extends FragmentPagerAdapter{
-        private static int NUM_ITEMS = 3;
+                markerOptions
+                        .position(new LatLng(lat,lon))
+                        .title(name);
 
-        public MyPagerAdapter(FragmentManager fragmentManager) {
-            super (fragmentManager);
-        }
-        @Override
-        public int getCount(){
-            return NUM_ITEMS;
-        }
+                BitmapDrawable bitmapDrawable=(BitmapDrawable)getResources().getDrawable(R.drawable.marker);//마커변경
+                Bitmap b = bitmapDrawable.getBitmap();
+                Bitmap smallMarker = Bitmap.createScaledBitmap(b, 100, 100, false);
+                markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
 
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return FirstFragment.newInstance(0, "Page # 1");
-                case 1:
-                     return SecondFragment.newInstance(1, "Page # 2");
-                case 2:
-                    return  ThirdFragment.newInstance(2, "Page # 3");
-                default:
-                    return null;
+                map.addMarker(markerOptions);
             }
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position){
-            return "Page " + position;
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 
+    private String getJsonString() {
+        String json = "";
+
+        try {
+            InputStream is = getAssets().open("Cathedral.json");
+            int fileSize = is.available();
+
+            byte[] buffer = new byte[fileSize];
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return json;
+    }
+
+    private void startLocationService() {
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            int chk1 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+            int chk2 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+            Location location = null;
+            if (chk1 == PackageManager.PERMISSION_GRANTED && chk2 == PackageManager.PERMISSION_GRANTED) {
+                location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            } else {
+                return;
+            }
+
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                String msg = "최근 위치 ->  Latitue : " + latitude + "\nLongitude : " + longitude;
+                showCurrentLocation(latitude, longitude);
+            }
+
+            GPSListener gpsListener = new GPSListener();
+            long minTime = 10000;
+            float minDistance = 0;
+
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, gpsListener);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class GPSListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            String message = "내 위치 -> Latitude : " + latitude + "\nLongitude:" + longitude;
+            Log.d("Map", message);
+
+            showCurrentLocation(latitude, longitude);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    }
+
+    private void showCurrentLocation(Double latitude, Double longitude) {
+        LatLng curPoint = new LatLng(latitude, longitude);
+
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+
+        //디폴트 위치, Seoul
+        Toast.makeText(getApplicationContext(), "실행", Toast.LENGTH_LONG).show();
+        String markerTitle = "내위치";
+        String markerSnippet = "위치정보가 확인되었습니다.";
+
+        if (currentMarker != null) currentMarker.remove();
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(curPoint);
+        markerOptions.title(markerTitle);
+        markerOptions.snippet(markerSnippet);
+        markerOptions.draggable(true);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        currentMarker = map.addMarker(markerOptions);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(curPoint, 15);
+        map.moveCamera(cameraUpdate);
+    }
+
+    @Override
+    public void onDenied(int i, String[] strings) {
+    }
+
+    @Override
+    public void onGranted(int i, String[] strings) {
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        AutoPermissions.Companion.parsePermissions(this, requestCode, permissions, this);
+    }
 
 }
+
+
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
