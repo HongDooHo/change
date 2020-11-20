c3264f8be8f32b7f7e3a5246f320599fd9804a3a
hyj6125 <0312jj@naver.com>
2020-11-21 07:24:00 +0900

    kakao login to main activity, 현재위치마커 최종수정

--- app/src/main/java/com/hufs/change/MainActivity.java
+++ app/src/main/java/com/hufs/change/MainActivity.java
@@ -1,22 +1,61 @@
 package com.hufs.change;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import me.relex.circleindicator.CircleIndicator;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-
-import android.os.Bundle;
-
-import com.hufs.change.R;
-
 import com.hufs.change.FirstFragment;
 import com.hufs.change.SecondFragment;
 import com.hufs.change.ThirdFragment;
 
-import me.relex.circleindicator.CircleIndicator;
+public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
 
-public class MainActivity extends AppCompatActivity {
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+    private Marker currentMarker = null;
 
     FragmentPagerAdapter adapterViewPager;
 
@@ -27,15 +66,212 @@ public class MainActivity extends AppCompatActivity {
 
         ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
         adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
-        vpPager.setAdapter(adapterViewPager);                                       
+        vpPager.setAdapter(adapterViewPager);
 
         CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
         indicator.setViewPager(vpPager);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+
+                // style
+                try {
+                    // Customise the styling of the base map using a JSON object defined
+                    // in a raw resource file.
+                    boolean success = googleMap.setMapStyle(
+                            MapStyleOptions.loadRawResourceStyle(
+                                    MainActivity.this, R.raw.style_json));
+                    if (!success) {
+                        Log.e("MapActivity", "Style parsing failed.");
+                    }
+                } catch (Resources.NotFoundException e) {
+                    Log.e("MapActivity", "Can't find style. Error: ", e);
+                }
+
+
+                map = googleMap;
+                startLocationService();
+                try {
+                    JSONArray jsonArray = new JSONArray(getJsonString());
+                    for(int i =0;i<jsonArray.length();i++) {
+                        JSONObject jObject = jsonArray.getJSONObject(i);
+                        String name = jObject.getString("name");
+                        int lon = jObject.getInt("longitude");
+                        int lat = jObject.getInt("latitude");
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions
+                                .position(new LatLng(lat,lon))
+                                .title(name);
+
+                        BitmapDrawable bitmapDrawable=(BitmapDrawable)getResources().getDrawable(R.drawable.marker);//마커변경
+                        Bitmap b=bitmapDrawable.getBitmap();
+                        Bitmap smallMarker = Bitmap.createScaledBitmap(b, 100, 100, false);
+                        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));
+
+                        map.addMarker(markerOptions);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try {
+            MapsInitializer.initialize(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        AutoPermissions.Companion.loadAllPermissions(this, 100);
+
+    }
+
+    private String getJsonString()
+    {
+        String json = "";
+
+        try {
+            InputStream is = getAssets().open("Cathedral.json");
+            int fileSize = is.available();
+
+            byte[] buffer = new byte[fileSize];
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return json;
     }
 
 
-    public static class MyPagerAdapter extends FragmentPagerAdapter{
-        private static int NUM_ITEMS = 3;
+    private void setDefaultLocation() {
+        //디폴트 위치, Seoul
+        LatLng DEFAULT_LOCATION = new LatLng(37.56, 126.97);
+        String markerTitle = "위치정보 가져올 수 없음";
+        String markerSnippet = "위치 퍼미션과 GPS 활성 요부 확인하세요";
+
+
+        if (currentMarker != null) currentMarker.remove();
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(DEFAULT_LOCATION);
+        markerOptions.title(markerTitle);
+        markerOptions.snippet(markerSnippet);
+        markerOptions.draggable(true);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        currentMarker = map.addMarker(markerOptions);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, 10);
+        map.moveCamera(cameraUpdate);
+    }
+
+    private void startLocationService() {
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            int chk1 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+            int chk2 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+            Location location = null;
+            if (chk1 == PackageManager.PERMISSION_GRANTED && chk2 == PackageManager.PERMISSION_GRANTED) {
+                location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            } else {
+                return;
+            }
+
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                String msg = "최근 위치 ->  Latitue : " + latitude + "\nLongitude : " + longitude;
+                showCurrentLocation(latitude, longitude);
+            }
+
+            GPSListener gpsListener = new GPSListener();
+            long minTime = 10000;
+            float minDistance = 0;
+
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, gpsListener);
+
+
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class GPSListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            String message = "내 위치 -> Latitude : " + latitude + "\nLongitude:" + longitude;
+            Log.d("Map", message);
+
+            showCurrentLocation(latitude, longitude);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    }
+
+    private void showCurrentLocation(Double latitude, Double longitude) {
+        LatLng curPoint = new LatLng(latitude, longitude);
+
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+
+        //디폴트 위치, Seoul
+        Toast.makeText(getApplicationContext(), "실행", Toast.LENGTH_LONG).show();
+        String markerTitle = "내위치";
+        String markerSnippet = "위치정보가 확인되었습니다.";
+
+        if (currentMarker != null) currentMarker.remove();
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(curPoint);
+        markerOptions.title(markerTitle);
+        markerOptions.snippet(markerSnippet);
+        markerOptions.draggable(true);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        currentMarker = map.addMarker(markerOptions);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(curPoint, 15);
+        map.moveCamera(cameraUpdate);
+    }
+
+    @Override
+    public void onDenied(int i, String[] strings) {
+    }
+
+    @Override
+    public void onGranted(int i, String[] strings) {
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        AutoPermissions.Companion.parsePermissions(this, requestCode, permissions, this);
+    }
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 2;
 
         public MyPagerAdapter(FragmentManager fragmentManager) {
             super (fragmentManager);
@@ -51,7 +287,7 @@ public class MainActivity extends AppCompatActivity {
                 case 0:
                     return FirstFragment.newInstance(0, "Page # 1");
                 case 1:
-                     return SecondFragment.newInstance(1, "Page # 2");
+                    return SecondFragment.newInstance(1, "Page # 2");
                 case 2:
                     return  ThirdFragment.newInstance(2, "Page # 3");
                 default:
@@ -64,4 +300,5 @@ public class MainActivity extends AppCompatActivity {
             return "Page " + position;
         }
     }
+
 }
